ecc118414d9140243130270bd9cedab0cc14ec19
Konrad Krawczyk <k.krawczyk@femax.pl>
2021-01-23 14:00:34 +0100

    added dynamic icon swapping

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,24 +2,25 @@ import React, { useState, useEffect } from 'react';
 import "./Sidebar.css";
 import {SidebarData} from './SidebarData';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 import Avatar from './img/src_avatar.svg'
+import bxMenu from '@iconify-icons/bx/bx-menu';
 function Sidebar() {
 
-    const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(true);
 
     return(
-        <body id="body-pd">
-        <header className="header" id="header">
+        <body id="body-pd" className={ `${visible ? 'body-pd' : '' }`}>
+        <header className={ `${visible ? 'header body-pd' : "header" }`} id="header">
             <div className="header__toggle" onClick={() => setVisible(prev => !prev)}>
-                <i className='bx bx-menu' id="header-toggle"><MenuIcon/></i>
-                {visible ? 'TRUE' : 'FALSE'}
+                <i className='bx bx-menu' id="header-toggle"> {visible ? <MenuIcon/> : <CloseIcon/> }</i>
             </div>
 
             <div className="header__img">
                 <img src={Avatar} alt=""/>
             </div>
         </header>
-        <div className="l-navbar" id="nav-bar">
+        <div className={ `${visible ? 'l-navbar show' : "l-navbar"}`} id="nav-bar">
         <nav className= "nav" >
         <div className="Sidebar">
             <ul className="SidebarList">
