56f85e5ca88bfa30f42b8601360415202c464623
Konrad Krawczyk <k.krawczyk@femax.pl>
2021-01-23 09:57:59 +0100

    Created sidebar

--- src/App.js
+++ src/App.js
@@ -1,45 +1,66 @@
 import React, {Component} from "react";
+import "./App.css";
 import Users from "./Users";
-class App extends Component {
-
-    state =
-        {
-            data: [],
-            name: "elo",
-            languages: ["1", "2", "3"]
-        }
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Sidebar from "./components/Sidebar";
 
 
-        componentDidMount() {
-        fetch('Http://localhost:8080/api/users')
-            .then(response => response.json())
-            .then(data =>{
-                console.log(data);
-                this.setState({data})
-            })
-        }
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload .
-                    </p>
-                    <div>
-                        {this.state.data.map(users => <Users list={users}/>)}
-                    </div>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        );
-    }
+function App(){
+    return(
+        <div className={"App"}>
+            <Sidebar />
+        </div>
+    )
 }
+// class App extends Component {
+//
+//     state =
+//         {
+//             data: [],
+//             name: "elo",
+//             languages: ["1", "2", "3"]
+//         }
+
+
+        // componentDidMount() {
+        // fetch('Http://localhost:8080/api/users')
+        //     .then(response => response.json())
+        //     .then(data =>{
+        //         console.log(data);
+        //         this.setState({data})
+        //     })
+        // }
+
+    // render() {
+    //     return (
+            // <div className="App">
+            //     <header className="App-header">
+            //         <p>
+            //             Edit <code>src/App.js</code> and save to reload .
+            //         </p>
+            //        {/*<div>*/}
+          //          {/*    {this.state.data.map(users => <Users list={users}/>)}*/}
+          //          {/*</div>*/}
+          //           <>
+          //           <Router>
+          //              <Switch>
+          //                  <Route path='/' />
+          //              </Switch>
+          //
+          //           <Navbar></Navbar>
+          //           </Router>
+          //           </>
+            //         <a
+            //             className="App-link"
+            //             href="https://reactjs.org"
+            //             target="_blank"
+            //             rel="noopener noreferrer"
+            //         >
+            //             Learn React
+            //         </a>
+            //     </header>
+            // </div>
+//         );
+//     }
+// }
 export default App;
