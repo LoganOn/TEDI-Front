b8d62dfe8227806b1012870b7c30b7957357aa30
Konrad Krawczyk <k.krawczyk@femax.pl>
2021-01-27 15:41:59 +0100

    done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Sidebar from "./components/Sidebar";
 
 function App(){
     return(
-        <div className={"App"}>
+        <div>
             <Sidebar />
         </div>
     )
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,7 @@ import {SidebarData} from './SidebarData';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
 import Avatar from './img/src_avatar.svg'
-import bxMenu from '@iconify-icons/bx/bx-menu';
+import Logo1 from './img/favicon1.ico'
 function Sidebar() {
 
     const [visible, setVisible] = useState(true);
@@ -22,18 +22,22 @@ function Sidebar() {
         </header>
         <div className={ `${visible ? 'l-navbar show' : "l-navbar"}`} id="nav-bar">
         <nav className= "nav" >
-        <div className="Sidebar">
-            <ul className="SidebarList">
+        <div>
+            <div className="nav__list">
+                <a className="nav__logo">
+                    <img className='nav__logo-icon' src={Logo1}></img>
+                    <span className="nav__logo-name">T-EDI</span>
+                </a>
             {SidebarData.map((val,key) =>{
                 return(
-                    <li className="Row" id={window.location.pathname == val.link ? "active" : ""} key={key} onClick={() => {window.location.pathname = val.link}}>
+                    <a className="nav__link" id={window.location.pathname == val.link ? "active" : ""} key={key} onClick={() => {window.location.pathname = val.link}}>
                         {" "}
-                        <div id = "icon">{val.icon}</div>{" "}
-                        <div id = "title">{val.title}</div>
-                    </li>
+                        <div id = "nav__icon">{val.icon}</div>
+                        <div id = "nav__name">{val.title}</div>
+                    </a>
                 )
             })}
-            </ul>
+            </div>
         </div>
         </nav>
         </div>
