72844626f732c946b597d0277a89c1e65e9181a1
Konrad Krawczyk <konrad.krawczyk@mayeryn.com>
2021-01-31 12:36:51 +0100

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, {Component} from "react";
 import Login from "./component/Login";
+import Sidebar from "./component/Sidebar";
 import {BrowserRouter as Router, HashRouter, Route} from "react-router-dom";
 import Users from "./component/Users";
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 70%;
+  left: 300px;
+  height: 70%;
+`
 
 class App extends Component {
 
     render() {
         return (
-            <Router>
-               <Route path='/' component={Login} />
-               <Route path='/home' component={Users} />
             <div>
-
-                <Login />
-            </div>
+            <Router>
+               <Route path='/' component={Sidebar} />
+               <Container>
+               <Route path='/home' component={Login} />
+               </Container>
             </Router>
+            </div>
         )
     }
 }
