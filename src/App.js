f2adf381fd76dbeb904d986afd6e426841b14d2e
Konrad Krawczyk <konrad.krawczyk@mayeryn.com>
2021-01-31 10:30:45 +0100

    added first route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, {Component} from "react";
 import Login from "./component/Login";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, HashRouter, Route} from "react-router-dom";
+import Users from "./component/Users";
 
 class App extends Component {
 
     render() {
         return (
+            <Router>
+               <Route path='/' component={Login} />
+               <Route path='/home' component={Users} />
             <div>
+
                 <Login />
             </div>
+            </Router>
         )
     }
 }
